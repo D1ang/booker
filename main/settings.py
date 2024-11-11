1348a2023883992799da50d6a22e4ccc42d919fc
Django Heimgartner <d.heimgartner@tudelft.nl>
2024-11-11 20:43:45 +0100

    Make frontpage available & reachable

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom build apps
+    'home',
+    'relations',
+    'quotation',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +49,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
