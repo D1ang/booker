05946f31fbd05068b490dfd62ea3da186485e5c0
Django Heimgartner <django.heimgartner@gmail.com>
2025-06-13 22:59:02 +0200

    Add quotation details page

--- quotation/urls.py
+++ quotation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import generate_quotation
+from .views import quotation_details, generate_quotation
 
 
 app_name = 'quotation'
 
 urlpatterns = [
+    path('details/<str:quotation_number>/', quotation_details, name='quotation_details'),
     path('generate/', generate_quotation, name='generate_quotation'),
 ]
--- quotation/views.py
+++ quotation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from datetime import datetime
 from weasyprint import HTML
+from .models import Quotation
 
 
 def generate_quotation(request: HttpRequest) -> HttpResponse:
@@ -19,3 +20,22 @@ def generate_quotation(request: HttpRequest) -> HttpResponse:
     # rendered_pdf = html.write_pdf('./static/invoice.pdf')
 
     return render(request, 'quotation/quotation.html', context)
+
+
+def quotation_details(request, quotation_number):
+    """Render the quotation details page."""
+    quotation = Quotation.objects.get(quotation_number=quotation_number)
+    quotation_rules = quotation.quotationrule_set.all()
+
+
+    print(quotation_rules)
+
+    for rule in quotation_rules:
+        print(rule.description, rule.amount)
+
+    context = {
+        'quotation': quotation,
+        'quotation_rules': quotation_rules,
+    }
+
+    return render(request, 'quotation/details.html', context)
